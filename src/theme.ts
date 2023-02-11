488fe07eac0c8c046d86c2f2e358041f13d73a7a
aarriiaan <aarriiaann@gmail.com>
2023-02-11 17:40:53 +0100

    fixed pallete colors for Light Mode

--- src/theme.ts
+++ src/theme.ts
@@ -5,38 +5,62 @@ export const themeTypes = {
   dark: "dark",
 };
 
+const lightPalette = {
+  primary: {
+    300: "#0C0C23",
+    400: "#FFFFFF",
+    500: "#EFF5FF",
+    600: "#FFBB5E",
+    610: "#FFF4EF",
+    700: "#FF6D93",
+    710: "#ECA3B6",
+    720: "#FFEFF3",
+    800: "#77777A",
+    810: "#8D8C8F",
+    820: "#ABAAAD",
+    830: "#EAE8ED",
+    840: "#F2F0F5",
+    850: "#FBFBFB",
+  },
+};
+
 export const lightTheme: DefaultTheme = {
   type: themeTypes.light,
   background: {
-    primary: "#ffffff ",
-    secondary: "#0C0C23",
+    primary: lightPalette.primary[400],
+    secondary: lightPalette.primary[300],
   },
   text: {
-    primary: "#0C0C23",
-    secondary: "#ABAAAD",
+    primary: lightPalette.primary[300],
+    secondary: lightPalette.primary[820],
   },
-  shadow: { primary: "0px 1px 0px #EAE8ED", secondary: "none" },
+  shadow: {
+    primary: `0px 1px 0px ${lightPalette.primary[830]}`,
+    secondary: "none",
+  },
   icons: {
-    primary: { background: "#EAE8ED", radius: "6px" },
-    secondary: { background: "#0C0C23", radius: "none" },
+    primary: { background: lightPalette.primary[830], radius: "6px" },
+    secondary: { background: lightPalette.primary[300], radius: "none" },
   },
   button: {
     primary: {
-      background: "#0C0C23",
-      color: "#FFFFFF",
+      background: lightPalette.primary[300],
+      color: lightPalette.primary[400],
       border: "none",
       radius: "6px",
       hover: {
-        background:
-          "linear-gradient(0deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1)), #0C0C23",
+        background: `linear-gradient(0deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1)), ${lightPalette.primary[300]}`,
       },
     },
     secondary: {
-      background: "#FFFFFF",
-      border: "1px solid #EAE8ED",
-      color: "#77777A",
+      background: lightPalette.primary[400],
+      border: `1px solid ${lightPalette.primary[830]}`,
+      color: lightPalette.primary[800],
       radius: "6px",
-      hover: { border: "1px solid #0C0C23", color: "#0C0C23" },
+      hover: {
+        border: `1px solid ${lightPalette.primary[300]}`,
+        color: lightPalette.primary[300],
+      },
     },
   },
 };
@@ -44,16 +68,19 @@ export const lightTheme: DefaultTheme = {
 export const darkTheme: DefaultTheme = {
   type: themeTypes.dark,
   background: {
-    primary: "#ffffff ",
-    secondary: "#0C0C23",
+    primary: lightPalette.primary[400],
+    secondary: lightPalette.primary[300],
   },
   text: {
-    primary: "#0C0C23",
-    secondary: "#ABAAAD",
+    primary: lightPalette.primary[300],
+    secondary: lightPalette.primary[820],
   },
-  shadow: { primary: "0px 1px 0px #EAE8ED", secondary: "none" },
+  shadow: {
+    primary: `0px 1px 0px ${lightPalette.primary[830]}`,
+    secondary: "none",
+  },
   icons: {
-    primary: { background: "#EAE8ED", radius: "6px" },
-    secondary: { background: "#EAE8ED", radius: "none" },
+    primary: { background: lightPalette.primary[830], radius: "6px" },
+    secondary: { background: lightPalette.primary[830], radius: "none" },
   },
 };
